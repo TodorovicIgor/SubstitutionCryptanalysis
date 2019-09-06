3a77aa6017311a8b7856419d8e86d17f4ab29047
Igor Todorovic <todorovicigor@hotmail.com>
2019-09-06 18:10:49 +0200

    code cleaning

--- cryptanalysis/monoalphabetic/analyzer.py
+++ cryptanalysis/monoalphabetic/analyzer.py
@@ -22,23 +22,15 @@ class Analyzer:
         iteration_best_difference = self.best_difference
         iteration_best_distribution = copy.copy(self.distribution)
         iteration_best_key = copy.copy(self.key)
+
         a = 0
         print("Starting difference is ", self.best_difference, "key is", self.key.key)
-
         while True:
-            # print("Current key is ", self.key.get_key())
-            # print(new_key.get_key(), ", ", self.key.get_key())
             for char in mapping.freq:
 
                 new_key = copy.copy(self.key)
                 new_key.swap(a, mapping.alphabet.find(char))
                 print(new_key.get_key())
-                # if a == 26:
-                #     b += 1
-                #     a = 0
-                #     if b == 26:
-                #         break
-                # ********************
                 # a += 1
                 # if a+b > 25:
                 #     a = 0
@@ -47,14 +39,14 @@ class Analyzer:
                 #         print("Done")
                 #         break
                 new_distribution = copy.copy(self.distribution)
-                new_distribution.swap_cols_rows(mapping.alphabet[a], char)  # ***************************
+                new_distribution.swap_cols_rows(mapping.alphabet[a], char)
                 current_difference = new_distribution.eval_difference(self.expected, new_key.get_key())
                 if current_difference < iteration_best_difference:
                     print("Found better key:", new_key.get_key(), ", better difference is", current_difference)
                     iteration_best_difference = copy.copy(current_difference)
                     iteration_best_distribution = copy.copy(new_distribution)
                     iteration_best_key = copy.copy(new_key)
-            # iteration done
+            # iteration done, found best place for character mapping.alphabet[a]
             if iteration_best_difference < self.best_difference:
                 self.best_difference = iteration_best_difference
                 self.distribution = copy.copy(iteration_best_distribution)
@@ -68,8 +60,7 @@ class Analyzer:
         print("text is", cipher.decrypt_mono(self.ciphertext, self.key.get_key()))
 
 plaintext = "Friendship contrasted solicitude insipidity in introduced literature it. ""He seemed denote except as oppose do spring my. ""Between any may mention evening age shortly can ability regular. He shortly sixteen of colonel colonel ""evening cordial to. Although jointure an my of mistress servants am weddings. Age why the therefore ""education unfeeling for arranging. Above again money own scale maids ham least led. Returned settling"" produced strongly ecstatic use yourself way. Repulsive extremity enjoyment she perceived nor.""Ladyship it daughter securing procured or am moreover mr. Put sir she exercise vicinity c""heerful wondered. Continual say suspicion provision you neglected sir curiosity unwilling. Simplicity ""end themselves increasing led day sympathize yet. General windows effects not are drawing man garrets. ""Common indeed garden you his ladies out yet. Preference imprudence contrasted to remarkably in on. ""Taken now you him trees tears any. Her object giving end sister except oppose. "
-ciphertext1 = file_reader.read_file("ciphertext1.txt")
+# ciphertext1 = file_reader.read_file("ciphertext1.txt")
 ciphertext = cipher.encrypt_mono(plaintext, "bcadefghijklmnopqrstuvwxyz")
-# a = Analyzer(cipher.transform(plaintext))
 a = Analyzer(ciphertext)
 a.break_cipher()
