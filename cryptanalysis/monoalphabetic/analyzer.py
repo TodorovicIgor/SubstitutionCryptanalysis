3e0b1fde9c73879459f07fb21dc1a296b48b92fd
TodorovicIgor <TodorovicIgor@hotmail.com>
2019-09-13 14:51:39 +0200

    cleaned code

--- cryptanalysis/monoalphabetic/analyzer.py
+++ cryptanalysis/monoalphabetic/analyzer.py
@@ -9,60 +9,45 @@ class Analyzer:
         self.expected = freq_matrix.FreqMatrix()
         self.expected.load_expected_bigram_file()
         self.distribution = freq_matrix.FreqMatrix()
-        self.key = key.Key(mapping.alphabet)
+        self.key = key.Key(mapping.freq)  # step 1
         self.plaintext = None
         self.ciphertext = ciphertext
         self.current_difference = float('inf')
         self.best_difference = float('inf')
-        self.distribution.load_freq_from_text(self.ciphertext)
+        self.distribution.load_freq_from_text(cipher.decrypt_mono(self.ciphertext, self.key.get_key()))  # step 2
 
     def break_cipher(self):
         print("Cipher is", self.ciphertext)
-        self.best_difference = self.distribution.eval_difference(self.expected, self.key.get_key())
-        iteration_best_difference = self.best_difference
-        iteration_best_distribution = copy.copy(self.distribution)
-        iteration_best_key = copy.copy(self.key)
-        a = 0
-        print("Starting difference is ", self.best_difference, "key is", self.key.key)
-
+        self.best_difference = self.distribution.eval_difference(self.expected, self.key.get_key())  # step 3
+        a = b = 0  # step 0
+        print("Starting function value is ", self.best_difference, "key is", self.key.key)
         while True:
-            # print("Current key is ", self.key.get_key())
-            # print(new_key.get_key(), ", ", self.key.get_key())
-            for char in mapping.freq:
+            print("Current key is \t", self.key.get_key())
+            new_key = copy.copy(self.key)  # step 4
+            new_distribution = copy.copy(self.distribution)  # step 5
 
-                new_key = copy.copy(self.key)
-                new_key.swap(a, mapping.alphabet.find(char))
-                print(new_key.get_key())
-                # if a == 26:
-                #     b += 1
-                #     a = 0
-                #     if b == 26:
-                #         break
-                # ********************
-                # a += 1
-                # if a+b > 25:
-                #     a = 0
-                #     b += 1
-                #     if b == 25:
-                #         print("Done")
-                #         break
-                new_distribution = copy.copy(self.distribution)
-                new_distribution.swap_cols_rows(mapping.alphabet[a], char)  # ***************************
-                current_difference = new_distribution.eval_difference(self.expected, new_key.get_key())
-                if current_difference < iteration_best_difference:
-                    print("Found better key:", new_key.get_key(), ", better difference is", current_difference)
-                    iteration_best_difference = copy.copy(current_difference)
-                    iteration_best_distribution = copy.copy(new_distribution)
-                    iteration_best_key = copy.copy(new_key)
-            # iteration done
-            if iteration_best_difference < self.best_difference:
-                self.best_difference = iteration_best_difference
-                self.distribution = copy.copy(iteration_best_distribution)
-                self.key = copy.copy(iteration_best_key)
-                a = -1
-            a += 1
-            if a == 26:
-                break
+            # alpha = mapping.freq[a]
+            # beta = mapping.freq[a+b]
+            new_key.swap(a, a+b)    # step 6a
+            a = a + 1               # step 6b
+            if a+b >= 26:           # step 6c
+                a = 1               # step 6d
+                b = b + 1           # step 6e
+                if b == 25: break   # step 6f
+
+            print(a, b)
+            print("New key is \t\t", new_key.get_key(), "swapped", mapping.freq[a], "and", mapping.freq[a+b])
+            new_distribution.swap_cols_rows(mapping.freq[a], mapping.freq[a+b])  # step 7
+            current_difference = new_distribution.eval_difference(self.expected, new_key.get_key())  # step 8
+            print("New function value is", current_difference, "with net", self.best_difference-current_difference)
+            if current_difference < self.best_difference:  # step 9
+                a = b = 0  # step 9a
+                print("Found better key:", new_key.get_key(), ", better function value is", current_difference)
+                self.best_difference = copy.copy(current_difference)    # step 10
+                self.key = copy.copy(new_key)                           # step 11
+                self.distribution = copy.copy(new_distribution)         # step 12
+        # step 13
+
         print("Best key is:", self.key.get_key())
         print("Best difference is", self.best_difference)
         print("text is", cipher.decrypt_mono(self.ciphertext, self.key.get_key()))
