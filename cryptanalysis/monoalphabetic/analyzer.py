d106bd059f12ca335ce6375346b34cef00d170a5
TodorovicIgor <TodorovicIgor@hotmail.com>
2019-09-17 15:12:22 +0200

    from work, finding bug

--- cryptanalysis/monoalphabetic/analyzer.py
+++ cryptanalysis/monoalphabetic/analyzer.py
@@ -16,10 +16,11 @@ class Analyzer:
         self.best_difference = float('inf')
         self.distribution.load_freq_from_text(cipher.decrypt_mono(self.ciphertext, self.key.get_key()))  # step 2
         # self.distribution.load_freq_from_text(self.ciphertext)  # step 2
+        self.expected.print_matrix()
 
     def break_cipher(self):
         print("Cipher is", self.ciphertext)
-        self.best_difference = self.distribution.eval_difference(self.expected, self.key.get_key())  # step 3
+        self.best_difference = self.distribution.eval_difference(self.expected)  # step 3
         # a = b = 0  # step 0
         print("Starting function value is ", self.best_difference, "key is", self.key.key)
         new_iteration = True
@@ -28,15 +29,21 @@ class Analyzer:
             print("started new iteration\n\n\n\n")
             for char1 in mapping.freq:
                 for char2 in mapping.freq:
+                    if char1 == 'q' and char2 == 'q':
+                        # print(self.distribution.print_matrix())
+                        pass
                     print("\nCurrent key is \t", self.key.get_key())
                     new_key = copy.copy(self.key)  # step 4
                     new_distribution = copy.copy(self.distribution)  # step 5
 
-                    new_key.swap(mapping.freq.find(char1), mapping.freq.find(char2))    # step 6a
+                    new_key.swap(new_key.get_key().find(char1), new_key.get_key().find(char2))    # step 6a
 
                     print("New key is \t\t", new_key.get_key(), "swapped", char1, "and", char2)
                     new_distribution.swap_cols_rows(char1, char2)  # step 7
-                    current_difference = new_distribution.eval_difference(self.expected, new_key.get_key())  # step 8
+                    if char1 == 'e' and char2 == 'e':
+                        self.expected.print_matrix()
+                        current_difference = new_distribution.eval_difference_test(self.expected)  # step 8
+                    current_difference = new_distribution.eval_difference(self.expected)  # step 8
                     print("New function value is", current_difference, "with net", self.best_difference-current_difference)
                     if current_difference < self.best_difference:  # step 9
                         # a = b = 0  # step 9a
@@ -46,16 +53,13 @@ class Analyzer:
                         self.key = copy.copy(new_key)                           # step 11
                         self.distribution = copy.copy(new_distribution)         # step 12
         # step 13
-
-        print(self.distribution.matrix)
-
         print("Best key is:", self.key.get_key())
         print("Best difference is", self.best_difference)
         print("text is", cipher.decrypt_mono(self.ciphertext, self.key.get_key()))
 
 
-# plaintext2 = file_reader.read_file("plaintext2.txt")
-# ciphertext2 = cipher.encrypt_mono(plaintext2, "abcdefghijklmnopqrstuvwxyz")
-# # a = Analyzer(cipher.transform(plaintext))
-# a = Analyzer(ciphertext2)
-# a.break_cipher()
+# TODO swap e sa e daje net == 0, swap q sa q daje net != 0
+plaintext2 = file_reader.read_file("plaintext1.txt")
+ciphertext2 = cipher.encrypt_mono(plaintext2, mapping.alphabet)
+a = Analyzer(ciphertext2)
+a.break_cipher()
--- cryptanalysis/monoalphabetic/freq_matrix.py
+++ cryptanalysis/monoalphabetic/freq_matrix.py
@@ -13,25 +13,19 @@ class FreqMatrix:
     def __init__(self):
         self.hashed = None
         self.matrix = np.zeros((26, 26), dtype=float)
-        self.indexes = freq
-
-    def swap_indexes(self, char1, char2):
-        lst = list(self.indexes)
-        lst[self.indexes.find(char1)], lst[self.indexes.find(char2)] = lst[self.indexes.find(char2)], lst[self.indexes.find(char1)]
-        self.indexes = ''.join(lst)
 
     def load_expected_bigram_file(self):
         temp = os.path.join(os.path.dirname(__file__), "../../", "data/", "two_gram_rel_freq.txt")
         self.hashed = json.loads(open(temp).readline())
         for k, v in self.hashed.items():
-            self.matrix[self.indexes.find(k[0]), self.indexes.find(k[1])] = float(v)
+            self.matrix[alphabet.find(k[0]), alphabet.find(k[1])] = float(v)
 
     def swap_cols_rows(self, char1, char2):
-        index1 = self.indexes.find(char1)
-        index2 = self.indexes.find(char2)
+        index1 = alphabet.find(char1)
+        index2 = alphabet.find(char2)
         self.matrix[:, [index1, index2]] = self.matrix[:, [index2, index1]]
         self.matrix[[index1, index2], :] = self.matrix[[index2, index1], :]
-        self.swap_indexes(char1, char2)
+        # self.swap_indexes(char1, char2)
 
     def load_freq_from_text(self, text):
         # treba da se proveri
@@ -47,28 +41,42 @@ class FreqMatrix:
         for k, v in hashed.items():
             prepared_ret.update({k: float(v/len(text))})
             # self.matrix[map.mapping[k[0]], map.mapping[k[1]]] = v/len(text)
-            self.matrix[self.indexes.find(k[0]), self.indexes.find(k[1])] = v/len(text)
+            self.matrix[alphabet.find(k[0]), alphabet.find(k[1])] = v/len(text)
         self.hashed = prepared_ret
 
-    def eval_difference(self, expected_matrix, key):
+    def eval_difference(self, expected_matrix):
         diff = 0
         for char1 in map.alphabet:
             for char2 in map.alphabet:
                 # diff += abs(self.matrix[self.indexes.find(decrypt_mono(char1, key)), self.indexes.find(decrypt_mono(char2, key))]-expected_matrix.matrix[expected_matrix.indexes.find(char1), expected_matrix.indexes.find(char2)])
-                diff += abs(self.matrix[self.indexes.find(char1), self.indexes.find(char2)]-expected_matrix.matrix[expected_matrix.indexes.find(char1), expected_matrix.indexes.find(char2)])
+                diff += abs(self.matrix[alphabet.find(char1), alphabet.find(char2)]-expected_matrix.matrix[alphabet.find(char1), alphabet.find(char2)])
+        return diff
+
+    def eval_difference_test(self, expected_matrix):
+        diff = 0
+        for char1 in map.alphabet:
+            for char2 in map.alphabet:
+                # diff += abs(self.matrix[self.indexes.find(decrypt_mono(char1, key)), self.indexes.find(decrypt_mono(char2, key))]-expected_matrix.matrix[expected_matrix.indexes.find(char1), expected_matrix.indexes.find(char2)])
+                print("char1 is", char1, ", char2 is", char2)
+                print("distr is ", self.matrix[alphabet.find(char1), alphabet.find(char2)], "expected is ", expected_matrix.matrix[alphabet.find(char1), alphabet.find(char2)])
+                print("difference is", self.matrix[alphabet.find(char1), alphabet.find(char2)]-expected_matrix.matrix[alphabet.find(char1), alphabet.find(char2)], "************************")
+                diff += abs(self.matrix[alphabet.find(char1), alphabet.find(char2)]-expected_matrix.matrix[alphabet.find(char1), alphabet.find(char2)])
+        print("sum is", diff)
         return diff
 
     def plaintext_suitable(self, file):
         self.load_freq_from_text(fr.read_file(file))
         expected = FreqMatrix()
         expected.load_expected_bigram_file()
-        print(self.eval_difference(expected, alphabet))
+        print(self.eval_difference(expected))
 
-    def print_indexes(self):
-        print(self.indexes)
-
+    # def print_indexes(self):
+    #     print(self.indexes)
+    #
     def print_matrix(self):
-        pprint.pprint(self.matrix)
+        for char1 in map.alphabet:
+            for char2 in map.alphabet:
+                print(char1, char2, self.matrix[alphabet.find(char1), alphabet.find(char2)])
 
 
 # t = FreqMatrix()
@@ -80,7 +88,7 @@ class FreqMatrix:
 # t.print_indexes()
 # t.print_matrix()
 
-e = FreqMatrix()
-e.load_expected_bigram_file()
-e.print_matrix()
-e.print_indexes()
+# e = FreqMatrix()
+# e.load_expected_bigram_file()
+# e.print_matrix()
+# e.print_indexes()
